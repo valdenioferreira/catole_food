062c997202d23e50ec693472a4b9081bd7cb213d
Valdenio Ferreira <valdenio.ferreira.dev@gmail.com>
2023-03-25 12:22:33 -0300

    Aplicando os metodos CRUD no projeto.

--- src/main/java/br/com/catole/food/dao/PratoDao.java
+++ src/main/java/br/com/catole/food/dao/PratoDao.java
@@ -12,8 +12,20 @@ public class PratoDao {
         this.entityManager = entityManager;
     }
 
-    public void cadastrar (Prato prato){
+    public void cadastrar(Prato prato) {
         this.entityManager.persist(prato);
         System.out.println("Entidade cadastrada:" + prato);
+    }
+
+    public Prato consultar(final Integer id){
+        return this.entityManager.find(Prato.class, id);
+    }
+
+    public void atualizar(final Prato prato){
+        this.entityManager.merge(prato);
+    }
+
+    public void exluir(final Prato prato){
+        this.entityManager.remove(prato);
     }
 }
